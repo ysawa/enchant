98aacefe0a5226af2136482f189d0b6cdf443b14
Y Sawa <yoshihirosawa@gmail.com>
2011-11-09 19:26:39 +0900

    modify UI

--- js/translation.js
+++ js/translation.js
@@ -6,7 +6,7 @@
     enchant_previous_text: '',
     enchant_insert: function (translated) {
       var text = this.enchant_get_selected_text();
-      var marker_id = "sel_" + new Date().getTime() + "_" + Math.random().toString().substr(2);
+      var marker_id = "enchant_" + new Date().getTime() + "_" + Math.random().toString().substr(2);
       if (text && this.enchant_previous_text != text) {
         var range = window.getSelection().getRangeAt(0).cloneRange();
 
@@ -17,23 +17,39 @@
 
         range.collapse(false);
 
-        marker = document.createElement("span");
+        marker = document.createElement("div");
         marker.id = marker_id;
-        marker.innerText = translated;
         range.insertNode(marker);
-        $(marker).addClass('enchant_translation');
-        $(marker).prepend('<span class="enchant_close">x</span>');
+        $(marker).addClass('enchant_insertion');
+        $(marker).append('<span class="enchant_close">x</span>');
+        $(marker).append('<span class="enchant_text">' + translated + '</span>');
+        $(marker).append('<textarea class="enchant_textarea">' + translated + '</textarea>');
 
         $(marker).hover(
           function () {
             $(this).children('span.enchant_close').show();
+            $(this).children('textarea.enchant_textarea').show();
+            $(this).children('textarea.enchant_textarea').autoResize({
+              onResize: function() {
+                  $(this).css({ opacity: 0.8 });
+              },
+              animateCallback: function() {
+                  $(this).css({ opacity: 1 });
+              },
+              animateDuration: 300,
+              extraSpace: 20
+            });
+            $(this).children('span.enchant_text').hide();
           },
           function () {
             $(this).children('span.enchant_close').hide();
+            $(this).children('span.enchant_text').show();
+            $(this).children('span.enchant_text').html($(this).children('textarea.enchant_textarea').val());
+            $(this).children('textarea.enchant_textarea').hide();
           }
         );
-        $(marker).click(function () {
-          $(this).remove();
+        $(marker).children('span.enchant_close').click(function () {
+          $(this).parent('div.enchant_insertion').remove();
         });
       }
       this.enchant_previous_text = text;
